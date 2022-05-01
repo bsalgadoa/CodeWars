fad89451294fc5bda8f89327f652e741e5f6890a
bsalgadoa <b.salgado.a@gmail.com>
2022-05-01 13:10:57 +0100

    done

--- exes_and_ohs.py
+++ exes_and_ohs.py
@@ -18,3 +18,17 @@ def xo(s):
             pass
 # return the comparisson:
     return x_count == o_count
+
+'''
+Now the cleaner and clevar solution:
+'''
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
+'''
+This is brilliant.
+Take string 's', lower all the characters makinf a lower case string.
+use count function to count the number of x and o and compare them to each other.
+Note: the count fucntion works in strings and lists so I didn't needed to convert my string into list, I could have used the count function directly"
+'''
