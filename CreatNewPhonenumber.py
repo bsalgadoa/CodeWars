eff8c64fde083440170c9a4ee7485bec7f854b53
bsalgadoa <b.salgado.a@gmail.com>
2022-05-02 17:43:54 +0100

    ok

---
+++ CreatNewPhonenumber.py
@@ -0,0 +1,43 @@
+
+'''
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+'''
+
+def create_phone_number(n):
+
+    #create split lists acording to the number positions
+    first3 = n[:3]
+    second3 = n[3:6]
+    last4 = n[-4:]
+
+    #create empty strings in order to save the elements of the splited lists
+    first3_str = ""
+    second3_str = ""
+    last4_str = ""
+
+    #convert the elements in splited list into string
+    for i in first3:
+        first3_str += str(i)
+    for i in second3:
+        second3_str += str(i)
+    for i in last4:
+        last4_str += str(i)
+
+    #combine each string above into phone number format
+    return "({}) {}-{}".format(first3_str, second3_str, last4_str)
+
+
+
+'''
+Above is my begginer solution.
+Now the freakin clever and awsome solution!!
+
+def create_phone_number(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+'''
--- test.py
+++ test.py
@@ -1,12 +1,31 @@
+#
+#
+# lst = [1, 2, 5]
+#
+# print(lst[-2:])
+# print(lst[-2:-1])
+# print(lst[-1:])
+#
+#
+# for i in lst[-2:-1]:
+#     new_lst_lenght = len(lst) + 3
+#     a = 0
+#     while a <= new_lst_lenght:
+#         lst.append(lst[-1] + lst[-2])
+#         a = len(lst) + 1
+# print(lst)
 
+#
+# def more_than_n (lst, item, n):
+#   if lst.count(item) > n:
+#     return True
+#   return False
+#
+# #Uncomment the line below when your function is done
+#
+# print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 0, 6))
+#
 
-n = 12345
-n = str(n)
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 12]
 
-(back_string) = n[::-1]
-print(back_string)
-print(type(back_string))
-
-print(list(back_string))
-list2=list(map(int,back_string))
-print("List of integers : ",list2)
+print("({}{}{}) {}{}{}-{}{}{}{}".format(*n))
