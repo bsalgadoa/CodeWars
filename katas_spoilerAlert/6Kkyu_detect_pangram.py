1a3f3182c61717d941a543761930de52d573efb7
bsalgadoa <b.salgado.a@gmail.com>
2022-05-30 18:02:48 +0100

    ok

---
+++ katas_spoilerAlert/6Kkyu_detect_pangram.py
@@ -0,0 +1,26 @@
+'''
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+'''
+
+import string
+
+def is_pangram(s):
+    for letter in string.ascii_lowercase:
+        if letter not in s.lower():
+            return False
+    return True
+
+
+''' cleaner solution: (not mine)
+
+import string
+
+def is_pangram(s):
+    return set(string.ascii_lowercase) <= set(s.lower())
+
+# <= means .issubset()
+# so what this is saying is to check if every single letter form a to z is in the sring 's' and return true or false.  
+
+'''
